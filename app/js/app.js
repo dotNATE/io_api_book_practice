5a1b9bbd6867f60648d539175f15d086b7a03c01
Nathaniel Payne <somersetnathaniel@gmail.com>
2021-04-22 19:44:09 +0100

    started getData function in app.js added fetches

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,8 @@
-console.log('test');
+async function getData(inputTitle) {
+    let response = fetch(`https://openlibrary.org/search.json?q=${inputTitle}`)
+    let template = fetch('template.hbs')
+    let json = response.json()
+
+    console.log(template.text())
+    console.log(json)
+}
--- app/js/dist/app.js
+++ app/js/dist/app.js
@@ -1,4 +1,33 @@
 "use strict";
 
-console.log('test');
+function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
+
+function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
+
+function getData(_x) {
+  return _getData.apply(this, arguments);
+}
+
+function _getData() {
+  _getData = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee(inputTitle) {
+    var response, template, json;
+    return regeneratorRuntime.wrap(function _callee$(_context) {
+      while (1) {
+        switch (_context.prev = _context.next) {
+          case 0:
+            response = fetch("https://openlibrary.org/search.json?q=".concat(inputTitle));
+            template = fetch('template.hbs');
+            json = response.json();
+            console.log(template.text());
+            console.log(json);
+
+          case 5:
+          case "end":
+            return _context.stop();
+        }
+      }
+    }, _callee);
+  }));
+  return _getData.apply(this, arguments);
+}
 //# sourceMappingURL=app.js.map
